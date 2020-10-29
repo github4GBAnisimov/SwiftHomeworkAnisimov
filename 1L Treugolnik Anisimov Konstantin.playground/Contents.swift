df8548cd2ccbdf120593565264de725ebf2852ca
Konstantin <mail4githubanisimov@yandex.ru>
2020-10-29 18:06:53 +0300

    lesson-1

---
+++ 1L Treugolnik Anisimov Konstantin.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+let a: Float = 5
+let b: Float = 5
+var c = sqrt(a*a+b*b)
+var per = a+b+c
+var S = (a*b)/2
+print ("Гипотенуза =", c, "Площадь треугольника =", S, "Периметр треугольника =", per)
